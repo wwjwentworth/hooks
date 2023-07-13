b9a3e4db141d5f18c427f038af07bac6edd80c13
wuwenjie <wuwenjie@moresec.com>
2023-07-13 08:08:53 +0800

    feat: useAutoRunPlugin源码学习

--- packages/hooks/src/createUpdateEffect/index.ts
+++ packages/hooks/src/createUpdateEffect/index.ts
@@ -3,6 +3,7 @@ import type { useEffect, useLayoutEffect } from 'react';
 
 type EffectHookType = typeof useEffect | typeof useLayoutEffect;
 
+// 功能等同于useEffect，但是会忽略首次执行，只在依赖更新的时候执行
 export const createUpdateEffect: (hook: EffectHookType) => EffectHookType =
   (hook) => (effect, deps) => {
     const isMounted = useRef(false);
--- packages/hooks/src/useRequest/src/plugins/useAutoRunPlugin.ts
+++ packages/hooks/src/useRequest/src/plugins/useAutoRunPlugin.ts
@@ -10,19 +10,27 @@ const useAutoRunPlugin: Plugin<any, any[]> = (
   const hasAutoRun = useRef(false);
   hasAutoRun.current = false;
 
+  // useUpdateEffect 等同于useEffect，只不过会忽略首次执行，只在依赖更新的时候执行
   useUpdateEffect(() => {
+    // 如果非手动执行 且 ready 设置为true
     if (!manual && ready) {
+      // 设置hasAutoRun为true
       hasAutoRun.current = true;
+      // 开始执行请求实例的run方法，并传入默认参数
       fetchInstance.run(...defaultParams);
     }
   }, [ready]);
 
+  // 对refreshDeps进行监听
   useUpdateEffect(() => {
+    // 如果hasAutoRun的值已经为true了，跳过这个步骤
     if (hasAutoRun.current) {
       return;
     }
+    // 非手动执行的情况
     if (!manual) {
       hasAutoRun.current = true;
+      // 依赖变化的时候的处理逻辑，假如有传的话，就执行该逻辑，否则请求请求
       if (refreshDepsAction) {
         refreshDepsAction();
       } else {
@@ -31,7 +39,7 @@ const useAutoRunPlugin: Plugin<any, any[]> = (
     }
   }, [...refreshDeps]);
 
-  // 如果ready设置为了true，请求不会被执行 stopNow 返回 true
+  // 如果ready设置为了false，请求不会被执行 stopNow 返回 true
   return {
     onBefore: () => {
       if (!ready) {
