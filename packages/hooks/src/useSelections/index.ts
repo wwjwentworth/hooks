e36b37b422386c9717cc6ac0ffe4b2d74b0fc4d8
wuwenjie <wuwenjie@moresec.com>
2023-07-30 17:41:49 +0800

    feat: useSelections源码学习

--- packages/hooks/src/useSelections/index.ts
+++ packages/hooks/src/useSelections/index.ts
@@ -4,6 +4,7 @@ import useMemoizedFn from '../useMemoizedFn';
 export default function useSelections<T>(items: T[], defaultSelected: T[] = []) {
   const [selected, setSelected] = useState<T[]>(defaultSelected);
 
+  // 学习：使用set
   const selectedSet = useMemo(() => new Set(selected), [selected]);
 
   const isSelected = (item: T) => selectedSet.has(item);
@@ -55,17 +56,17 @@ export default function useSelections<T>(items: T[], defaultSelected: T[] = []) 
   const toggleAll = () => (allSelected ? unSelectAll() : selectAll());
 
   return {
-    selected,
-    noneSelected,
-    allSelected,
-    partiallySelected,
-    setSelected,
-    isSelected,
-    select: useMemoizedFn(select),
-    unSelect: useMemoizedFn(unSelect),
-    toggle: useMemoizedFn(toggle),
-    selectAll: useMemoizedFn(selectAll),
-    unSelectAll: useMemoizedFn(unSelectAll),
-    toggleAll: useMemoizedFn(toggleAll),
+    selected, // 当前选中的
+    noneSelected, // 是否一个都没有选择
+    allSelected, // 是否全选
+    partiallySelected, // 是否部分选择
+    setSelected, // 设置当前选中的元素
+    isSelected, // 是否被选择
+    select: useMemoizedFn(select), // 选择单个元素
+    unSelect: useMemoizedFn(unSelect), // 取消选择单个元素
+    toggle: useMemoizedFn(toggle), // 反选单个元素
+    selectAll: useMemoizedFn(selectAll), // 全选
+    unSelectAll: useMemoizedFn(unSelectAll), // 取消全选
+    toggleAll: useMemoizedFn(toggleAll), // 反选全部元素
   } as const;
 }
