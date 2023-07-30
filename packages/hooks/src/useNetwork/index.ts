fbea31d2161800f7d0130cba193bbedd0759930a
wuwenjie <wuwenjie@moresec.com>
2023-07-30 17:34:44 +0800

    feat: useNetwork源码学习

--- packages/hooks/src/useNetwork/index.ts
+++ packages/hooks/src/useNetwork/index.ts
@@ -13,9 +13,9 @@ export interface NetworkState {
 }
 
 enum NetworkEventType {
-  ONLINE = 'online',
-  OFFLINE = 'offline',
-  CHANGE = 'change',
+  ONLINE = 'online', // 在线
+  OFFLINE = 'offline', // 离线
+  CHANGE = 'change', // 网络波动
 }
 
 function getConnection() {
@@ -28,11 +28,17 @@ function getConnectionProperty(): NetworkState {
   const c = getConnection();
   if (!c) return {};
   return {
+    // 当前链接下的往返试演
     rtt: c.rtt,
+    // 返回设备正在与网络通信进行连接的类型（wifi/bluetooth等）
     type: c.type,
+    // 如果用户设备上设置了减少数据使用，返回true
     saveData: c.saveData,
+    // 有效带宽估值
     downlink: c.downlink,
+    // 最大下行速度
     downlinkMax: c.downlinkMax,
+    // 网络连接的类型，值有 'slow-2g', '2g', '3g', or '4g'.
     effectiveType: c.effectiveType,
   };
 }
