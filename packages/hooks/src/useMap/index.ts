635e5fd5fed7458eefd6367dd030b3335ee64276
wuwenjie <wuwenjie@moresec.com>
2023-08-06 19:01:42 +0800

    feat: useMap&useSet源码学习

--- packages/hooks/src/useMap/index.ts
+++ packages/hooks/src/useMap/index.ts
@@ -6,10 +6,12 @@ function useMap<K, T>(initialValue?: Iterable<readonly [K, T]>) {
     return initialValue === undefined ? new Map() : new Map(initialValue);
   };
 
+  // 通过Map构造函数创建map，如果有默认的值的话，传入默认值
   const [map, setMap] = useState<Map<K, T>>(() => getInitValue());
 
   const set = (key: K, entry: T) => {
     setMap((prev) => {
+      // 调用map的set方法
       const temp = new Map(prev);
       temp.set(key, entry);
       return temp;
@@ -23,6 +25,7 @@ function useMap<K, T>(initialValue?: Iterable<readonly [K, T]>) {
   const remove = (key: K) => {
     setMap((prev) => {
       const temp = new Map(prev);
+      // 调用map的delete方法
       temp.delete(key);
       return temp;
     });
--- packages/hooks/src/useSet/index.ts
+++ packages/hooks/src/useSet/index.ts
@@ -2,13 +2,16 @@ import { useState } from 'react';
 import useMemoizedFn from '../useMemoizedFn';
 
 function useSet<K>(initialValue?: Iterable<K>) {
+  // 获取默认值
   const getInitValue = () => {
     return initialValue === undefined ? new Set<K>() : new Set(initialValue);
   };
 
+  // 通过Set构造函数创建set
   const [set, setSet] = useState<Set<K>>(() => getInitValue());
 
   const add = (key: K) => {
+    // 如果set中已经有key了，就不再add
     if (set.has(key)) {
       return;
     }
@@ -20,6 +23,7 @@ function useSet<K>(initialValue?: Iterable<K>) {
   };
 
   const remove = (key: K) => {
+    // 如果set中没有key，就不调用delete方法
     if (!set.has(key)) {
       return;
     }
