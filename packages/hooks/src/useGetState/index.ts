213d9d97803bb934f9e72c40ebdb83a7e2f434fb
wuwenjie <wuwenjie@moresec.com>
2023-08-06 19:17:29 +0800

    feat: useGetState源码学习

--- packages/hooks/src/useGetState/index.ts
+++ packages/hooks/src/useGetState/index.ts
@@ -6,6 +6,7 @@ type GetStateAction<S> = () => S;
 function useGetState<S>(
   initialState: S | (() => S),
 ): [S, Dispatch<SetStateAction<S>>, GetStateAction<S>];
+
 function useGetState<S = undefined>(): [
   S | undefined,
   Dispatch<SetStateAction<S | undefined>>,
@@ -13,7 +14,9 @@ function useGetState<S = undefined>(): [
 ];
 function useGetState<S>(initialState?: S) {
   const [state, setState] = useState(initialState);
+  // 返回的 ref 对象在组件的整个生命周期内持续存在。
   const stateRef = useRef(state);
+  // 通过stateRef记录最新的值
   stateRef.current = state;
 
   const getState = useCallback(() => stateRef.current, []);
--- packages/hooks/src/useSafeState/index.ts
+++ packages/hooks/src/useSafeState/index.ts
@@ -7,9 +7,11 @@ function useSafeState<S>(initialState: S | (() => S)): [S, Dispatch<SetStateActi
 function useSafeState<S = undefined>(): [S | undefined, Dispatch<SetStateAction<S | undefined>>];
 
 function useSafeState<S>(initialState?: S | (() => S)) {
+  // 判断组件是否已经被卸载
   const unmountedRef = useUnmountedRef();
   const [state, setState] = useState(initialState);
   const setCurrentState = useCallback((currentState) => {
+    // 如果组件已经被卸载了，停止更新
     /** if component is unmounted, stop update */
     if (unmountedRef.current) return;
     setState(currentState);
