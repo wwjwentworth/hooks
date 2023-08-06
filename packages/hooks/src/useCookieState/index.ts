d86c28625d3cb2150c49670940447fd4e46498e1
wuwenjie <wuwenjie@moresec.com>
2023-08-06 18:52:24 +0800

    feat: useCookieState源码学习

--- packages/hooks/src/useCookieState/index.ts
+++ packages/hooks/src/useCookieState/index.ts
@@ -12,9 +12,10 @@ export interface Options extends Cookies.CookieAttributes {
 function useCookieState(cookieKey: string, options: Options = {}) {
   const [state, setState] = useState<State>(() => {
     const cookieValue = Cookies.get(cookieKey);
-
+    // 如果cookie里面有值，返回cookie里面的值
     if (isString(cookieValue)) return cookieValue;
 
+    // 否则返回options中的defaultValue
     if (isFunction(options.defaultValue)) {
       return options.defaultValue();
     }
@@ -27,14 +28,18 @@ function useCookieState(cookieKey: string, options: Options = {}) {
       newValue: State | ((prevState: State) => State),
       newOptions: Cookies.CookieAttributes = {},
     ) => {
+      // 合并options和newOptions
       const { defaultValue, ...restOptions } = { ...options, ...newOptions };
       setState((prevState) => {
         const value = isFunction(newValue) ? newValue(prevState) : newValue;
+        // 如果值不存在，清除cookie
         if (value === undefined) {
           Cookies.remove(cookieKey);
         } else {
+          // 否则调用js-cookie的set方法
           Cookies.set(cookieKey, value, restOptions);
         }
+        // 最终返回新值
         return value;
       });
     },
